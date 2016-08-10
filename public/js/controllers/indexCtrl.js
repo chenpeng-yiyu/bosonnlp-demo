33a3ea6c8d324a0b42856db5779f510d319bc94e
chenpeng1102 <1632179320@qq.com>
2016-08-11 00:11:38 +0800

    补充配置文件

--- public/js/controllers/indexCtrl.js
+++ public/js/controllers/indexCtrl.js
@@ -6,6 +6,7 @@ define(['app', 'config', 'dataService', 'lodash'], function(app, config) {
 		/**
 		 * 提交文本
 		 */
+		$scope.analysisText = config.defaultContent;
 		$scope.toAnalysis = function(e) {
 			dataService.getAnalysisData({
 				type: 'POST',
@@ -14,7 +15,7 @@ define(['app', 'config', 'dataService', 'lodash'], function(app, config) {
 				$scope.analysis = _.zip(data[0].tag, data[0].word);
 				var tag = _.uniq(data[0].tag);
 				var mean = tag.map(function(item) {
-					return config.wordMean[item] || '未知';  //可能还有某些词性在配置文件中不全
+					return config.wordMean[item] || '';  //可能还有某些词性在配置文件中不全
 				});
 				$scope.wordMeans = _.zip(tag, mean);
 			}, function(err) {
